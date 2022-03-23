8ae6e5d8e23e113afbb9d8084010f9e212a07c88
idn-fahmifn <fahmifn.idn@gmail.com>
2022-03-23 14:30:06 +0700

    update

--- routing/router/index.js
+++ routing/router/index.js
@@ -9,5 +9,24 @@ router.get("/", (req, res, next)=> {
     res.send('Goodbye, Mars!')
 }) 
 
+// route method POST
+router.post(
+    "/", 
+    bodyParser.json(),
+    bodyParser.urlencoded({extended:true}),
+    (req, res, next)=>{
+        res.send(req.body)
+    }
+)
+
+// route method DELETE
+router.delete("/:id", (req, res, next) => {
+    res.send(`Data dengan id ${req.params.id} telah dihapus`)
+})
+
+router.delete("/", (req, res, next)=>{
+    res.send(`Masukan dulu id nya`)
+})
+
 // export router agar bisa dipanggil di index utama (main index)
 module.exports = router
